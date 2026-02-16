253ec7a4162106cf8278be4ca52c1c27158b61ed
Maanvir Sidhu <maanvir2611@gmail.com>
2026-02-15 22:14:47 -0500

    Added var const usage example

--- 01-Basics/test.js
+++ 01-Basics/test.js
@@ -1,1 +1,20 @@
 console.log("Maanvir Sidhu")
+
+const accountId = "abc123"
+let accountEmail = "maanvir_89@gmail.com"
+var accountPassword = "12345"
+accountCity = "Amritsar"
+let accountState;
+
+accountEmail = "xyz@gmail.com"
+accountPassword = "67890"
+accountCity = "Jalandhar"
+
+// accountId = "xyz123" // This will throw an error because accountId is a constant and cannot be reassigned.
+
+/*
+Prefer using const for variables that won't be reassigned, and let for variables that will be reassigned. 
+Avoid using var to prevent issues with variable hoisting and scope.
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
